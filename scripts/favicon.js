547a1cd9d3f53acc9f89751a2a2b6d6e42fb11f4
sugarshin <shinsugar@gmail.com>
2016-12-04 23:50:03 +0900

    Tweak scripts

--- scripts/favicon.js
+++ scripts/favicon.js
@@ -1,10 +1,10 @@
-const fs = require('fs');
-const favicons = require('favicons');
-const toIco = require('to-ico');
-const chalk = require('chalk');
+const fs = require('fs')
+const favicons = require('favicons')
+const toIco = require('to-ico')
+const chalk = require('chalk')
 
-const source = './src/portrait.png';
-const outDir = 'build';
+const source = './src/portrait.png'
+const outDir = 'build'
 const configuration = {
   icons: {
     android: false,
@@ -16,19 +16,19 @@ const configuration = {
     opengraph: false,
     twitter: false,
     windows: false,
-    yandex: false
-  }
-};
-const target = 'favicon-32x32.png';
+    yandex: false,
+  },
+}
+const target = 'favicon-32x32.png'
 
 favicons(source, Object.assign({}, favicons.config, configuration), (err, res) => {
-  if (err) throw new Error(err);
-  const buffers = res.images.filter(({ name }) => name === target).map(i => i.contents);
+  if (err) throw new Error(err)
+  const buffers = res.images.filter(({ name }) => name === target).map(i => i.contents)
   toIco(buffers)
-    .then(buf => {
-      const path = `${outDir}/favicon.ico`;
-      fs.writeFileSync(path, buf);
-      console.log(`  ${chalk.bgGreen(' generate ')} ${chalk.gray(path)}`);
-    })
-    .catch(err => { throw new Error(err); });
-});
+  .then(buf => {
+    const path = `${outDir}/favicon.ico`
+    fs.writeFileSync(path, buf)
+    console.log(`  ${chalk.bgGreen(' generate ')} ${chalk.gray(path)}`)
+  })
+  .catch(err => { throw new Error(err) })
+})
--- scripts/imagemin.js
+++ scripts/imagemin.js
@@ -1,10 +1,10 @@
-const imagemin = require('imagemin');
-const pngquant = require('imagemin-pngquant');
-const chalk = require('chalk');
+const imagemin = require('imagemin')
+const pngquant = require('imagemin-pngquant')
+const chalk = require('chalk')
 
 imagemin(['src/*.png'], 'build', { plugins: [pngquant()] })
 .then(files => {
   files.forEach(({ path }) => {
-    console.log(`  ${chalk.bgGreen(' minified ')} ${chalk.gray(path)}`);
-  });
-});
+    console.log(`  ${chalk.bgGreen(' minified ')} ${chalk.gray(path)}`)
+  })
+})

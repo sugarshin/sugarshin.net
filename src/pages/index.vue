e63c9b3386f3d9145ee2b36afeb2b261587579bc
Shingo Sato <shinsugar@gmail.com>
2020-08-31 07:45:10 +0900

    Update index.vue

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -48,7 +48,7 @@ export default Vue.extend({
   data() {
     return {
       informations: [
-        { icon: 'terminal', text: 'Software Engineer 🧑‍💻' },
+        { icon: 'terminal', text: 'Software Engineer' },
         { icon: 'location', text: 'Tokyo, Japan' },
       ],
       links: [
